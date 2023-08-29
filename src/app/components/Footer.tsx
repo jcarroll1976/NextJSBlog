ff55fd70a7dd63a5754b50c72945c44d4cfb52f0
Joshua Carroll <carrolljosh1976@gmail.com>
2023-08-29 19:43:35 -0400

    added styles and images to footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import './Footer.css'
+import Image from 'next/image'
 
 type Props = {}
 
 function Footer({}: Props) {
   return (
-    <div>
+    <div className='footer-container'>
         <div>&copy;2023 Daily Blogs. All Rights Reserved.</div>
-        <div></div>
+        <div className='footer-social'>
+            <Image src="/1.png" alt="Daily Blogs Facebook" width={15} height={15} className='footer-icon' />
+            <Image src="/2.png" alt="Daily Blogs Instagram" width={15} height={15} className='footer-icon' />
+            <Image src="/3.png" alt="Daily Blogs Twitter" width={15} height={15} className='footer-icon' />
+            <Image src="/4.png" alt="Daily Blogs YouTube" width={15} height={15} className='footer-icon' />
+        </div>
     </div>
   )
 }
