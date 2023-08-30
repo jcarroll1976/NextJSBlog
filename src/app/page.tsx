04d79b288e7f7c110b57bd4ac04039b8d62e53a1
Joshua Carroll <carrolljosh1976@gmail.com>
2023-08-29 20:01:07 -0400

    added image to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Hero from "public/hero.png"
 
 export default function Home() {
   return (
     <div>
-      Home
+      <Image src={Hero} alt=''/>
     </div>
   )
 }
