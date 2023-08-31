7e6940fceb211f92fda2a3d15e8c95074a5d9a20
Joshua Carroll <carrolljosh1976@gmail.com>
2023-08-30 21:19:25 -0400

    added text and styles to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
 import Image from 'next/image'
-import styles from './page.module.css'
+import './page.css'
 import Hero from "public/hero.png"
 
 export default function Home() {
   return (
-    <div>
-      <Image src={Hero} alt=''/>
+    <div className='home-container'>
+      <div className='home-item'>
+        <h1 className='home-title'>Better Design for your digital products.</h1>
+        <p className='home-description'>Bringing your Ideas into Reality.We bring together the teams from 
+          the global tech industry.
+        </p>
+        <button className='home-button'>View Our Work</button>
+      </div>
+      <div className='home-item'>
+        <Image src={Hero} alt='' className='home-image' />
+      </div>
     </div>
   )
 }
