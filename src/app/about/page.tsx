03f65f30b7ce0264e55cab0096aa2474283b09e2
Joshua Carroll <carrolljosh1976@gmail.com>
2023-08-31 22:27:04 -0400

    added image and text to About page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,44 @@
+import Image from 'next/image'
 import React from 'react'
 
 type Props = {}
 
 function About({}: Props) {
   return (
-    <div>About</div>
+    <div className='about-container'>
+      <div className='aboutImage-container'>
+        <Image
+        src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+        fill={true}
+        alt=""
+        className="about-image"
+        />
+        <div className='about-image-text'>
+          <h1 className='about-image-title'>Digital Storytellers</h1>
+          <h2 className='about-image-description'>Handcrafting award winning digital experiences</h2>
+        </div>
+      </div>
+      <div className='aboutText-container'>
+        <div className='aboutText-item'>
+          <h1 className='aboutText-title'>Who are We?</h1>
+          <p className='aboutText-description'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+            quae dolor, optio voluptatibus magnam iure esse tempora beatae. A
+            suscipit eos. Animi quibusdam cum omnis officiis voluptatum quo ea
+            eveniet? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Ducimus quae dolor, optio voluptatibus magnam iure esse tempora
+            beatae, a suscipit eos. Animi quibusdam cum omnis officiis
+            <br />
+            <br />
+            voluptatum quo ea eveniet? Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Ducimus quae dolor, optio voluptatibus magnam iure
+            esse tempora beatae, a suscipit eos. Animi quibusdam cum omnis
+            officiis voluptatum quo ea eveniet?
+          </p>
+        </div>
+        <div className='aboutText-container-item'></div>
+      </div>
+    </div>
   )
 }
 
