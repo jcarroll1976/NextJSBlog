cdd08443aba48db60422bae8d74c8a41686b4fb2
Joshua Carroll <carrolljosh1976@gmail.com>
2023-09-04 22:43:19 -0400

    added image and contact form to contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,37 @@
 import React from 'react'
-import "./contact.css"
+import "./Contact.css"
+import Image from 'next/image'
+import Button from '../components/Button'
 
 type Props = {}
 
 function Contact({}: Props) {
   return (
-    <div className='contact'>Contact</div>
+    <div className='contact-container'>
+      <h1 className='contact-title'>Let's Keep in Touch</h1>
+      <div className='contact-content'>
+        <div className='contact-imgContainer'>
+          <Image
+          src="/contact.png"
+          alt=""
+          fill={true}
+          className="contact-image"
+          />
+        </div>
+        <form className='contact-form'>
+          <input type='text' placeholder='name' className='contact-input' />
+          <input type="email" placeholder='email' className='contact-input' />
+          <textarea
+          className='contact-textarea'
+          placeholder='Enter your message'
+          cols={30}
+          rows={10}
+          >
+          </textarea>
+          <Button url="#" text="Send" />
+        </form>
+      </div>
+    </div>
   )
 }
 
