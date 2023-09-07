9d4f90a355ab4d39428e55dd9bf7db703595a360
Joshua Carroll <carrolljosh1976@gmail.com>
2023-09-06 22:08:56 -0400

    started building portfolio page

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,10 +1,24 @@
+import Link from 'next/link'
 import React from 'react'
 
 type Props = {}
 
 function Portfolio({}: Props) {
   return (
-    <div>Portfolio</div>
+    <div className='portfolio-container'>
+      <h1 className='portfolio-title'>Choose a gallery</h1>
+      <div className='portfolio-items'>
+        <Link className='portfolio-item' href="/portfolio/illustrations">
+          <span className='portfolioItem-title'>Illustrations</span>
+        </Link>
+        <Link className='portfolio-item' href="/portfolio/websites">
+          <span className='portfolioItem-title'>Websites</span>
+        </Link>
+        <Link className='portfolio-item' href="/portfolio/applications">
+          <span className='portfolioItem-title'>Applications</span>
+        </Link>
+      </div>
+    </div>
   )
 }
 
