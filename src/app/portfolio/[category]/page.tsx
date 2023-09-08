0f546aa404563cfd6f5fafc0a3aa66e8f4017ed3
Joshua Carroll <carrolljosh1976@gmail.com>
2023-09-07 21:55:26 -0400

    added content for category

--- src/app/portfolio/[category]/page.tsx
+++ src/app/portfolio/[category]/page.tsx
@@ -1,10 +1,21 @@
+import { Params } from 'next/dist/shared/lib/router/utils/route-matcher';
 import React from 'react'
 
-type Props = {}
+type Props = {
+  params: Params;
+  //category: string;
+}
 
-function Category({}: Props) {
+function Category({params}: Props) {
+  console.log(params);
   return (
-    <div>Category</div>
+    <div className='category-container'>
+      <h1 className='category-title'>{params.category}</h1>
+      <div className='category-item'>
+        <div className='category-content'></div>
+        <div className='category-imgContainer'></div>
+      </div>
+    </div>
   )
 }
 
