bc309d9a9121dd4c29f9bab8ed7a480bb663e47b
Joshua Carroll <carrolljosh1976@gmail.com>
2023-09-11 21:47:57 -0400

    added text, author avatar, and author name to blog post

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import React from 'react'
 
 type Props = {}
@@ -7,7 +8,26 @@ function BlogPost({}: Props) {
     <div className='blogPost-container'>
       <div className='blogPost-top'>
         <div className='blogPost-info'>
-          
+          <h1 className='blogPost-title'>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis, eos.
+          </h1>
+          <p className='blogPost-description'>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam iste atque 
+            distinctio nihil deleniti ut omnis alias voluptatibus necessitatibus rerum 
+            officiis, quos exercitationem modi assumenda commodi veritatis dignissimos 
+            suscipit praesentium, quisquam nemo quibusdam odit culpa quasi beatae? 
+            Iusto, voluptates dignissimos.
+          </p>
+          <div className='blogPost-author'>
+            <Image
+            src="https://images.pexels.com/photos/3785077/pexels-photo-3785077.jpeg?auto=compress&cs=tinysrgb&w=600"
+            alt=""
+            width={40}
+            height={40}
+            className='blogPost-avatar'
+            />
+            <span className="blogPost-username">John Doe</span>
+          </div>
         </div>
       </div>
     </div>
