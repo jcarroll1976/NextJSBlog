709f323df3c072317da03e6ccee0be26dc2d4d6c
Joshua Carroll <carrolljosh1976@gmail.com>
2023-09-11 21:52:46 -0400

    added component to toggle dark mode

---
+++ src/app/components/DarkModeToggle.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./DarkModeToggle.css"
+
+type Props = {}
+
+function DarkModeToggle({}: Props) {
+  return (
+    <div>DarkModeToggle</div>
+  )
+}
+
+export default DarkModeToggle
